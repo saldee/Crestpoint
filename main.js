0d25e9fee5dd74b7dde0aa7d3b989119e67b2387
SALEH <saldee60@gmail.com>
2025-10-18 15:06:33 +0300

    Update main.js

--- main.js
+++ main.js
@@ -41,3 +41,13 @@ document.addEventListener("DOMContentLoaded", () => {
   renderIfEmpty("#contact-info", renderContactInfo);
   renderIfEmpty("#google-map", renderGoogleMap);
 });
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("menu-toggle");
+  const nav = document.getElementById("nav-links");
+
+  if (toggle && nav) {
+    toggle.addEventListener("click", () => {
+      nav.classList.toggle("show");
+    });
+  }
+});
