2180b9945fe84fa67c41590735890222db4ada15
SALEH <saldee60@gmail.com>
2025-10-28 15:27:42 +0300

    Update main.js

--- main.js
+++ main.js
@@ -74,3 +74,10 @@ document.addEventListener("DOMContentLoaded", () => {
     });
   }
 });
+// Mobile Hamburger Menu
+const menuToggle = document.getElementById("menu-toggle");
+const navLinks = document.getElementById("nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("show");
+});
